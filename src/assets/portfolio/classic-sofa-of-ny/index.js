4b1372d5d5cef8c61257fb5203359d0c5b430464
Harry Solovay <harrysolovay@gmail.com>
2018-08-08 11:03:50 -0400

    fixed broken link

--- src/assets/portfolio/classic-sofa-of-ny/index.js
+++ src/assets/portfolio/classic-sofa-of-ny/index.js
@@ -1,5 +1,5 @@
 export default {
-  url: 'http://bespokesofa.co',
+  url: 'https://classicsofa.com',
   thumbnail: require('./thumbnail.jpg'),
   title: `Classic Sofa of NY`,
   description: `Crafting furniture for NYC's hottest environments (including the Gansevoort, Midtown, and Royalton Hotels), Classic Sofa of New York needed a re-vamped online store. This is it.`,
