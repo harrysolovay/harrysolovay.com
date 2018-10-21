ce2dfff72c52003c34d664ef38bbd7dcd9686915
Harry Solovay <harrysolovay@gmail.com>
2018-10-21 19:38:10 -0400

    updated wording

--- src/assets/portfolio/state-mint/index.js
+++ src/assets/portfolio/state-mint/index.js
@@ -2,7 +2,7 @@ export default {
   url: 'https://github.com/harrysolovay/state-mint',
   thumbnail: require('./thumbnail.jpg'),
   title: `State Mint (lib)`,
-  description: `I created this open source state management and persistence library for React. It allows you to create a data layer with far greater simplicity than any alternative.`,
+  description: `I created this open source state management and persistence library for React. It allows you to create a data layer with far less boilerplate code than alternative libraries.`,
   category: 'DEV',
   date: 'September 2018',
 }
